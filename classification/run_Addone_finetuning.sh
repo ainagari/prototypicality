ba104eb51b0a1a2e57c17251a65c77f537cfc4a3
ainagari <49491477+ainagari@users.noreply.github.com>
2021-11-22 14:11:47 +0100

    Update run_Addone_finetuning.sh

--- classification/run_Addone_finetuning.sh
+++ classification/run_Addone_finetuning.sh
@@ -1,8 +1,8 @@
 
 ########################### BERT (CLS)
 
-MODEL_TYPE = "bert"
-MODEL_NAME = "bert-base-uncased"
+MODEL_TYPE="bert"
+MODEL_NAME="bert-base-uncased"
 
 #python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/cls_lr5e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_epochs --save_model --dataset addone --learning_rate 5e-5
 #python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/cls_lr3e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_epochs --save_model --dataset addone --learning_rate 3e-5
@@ -17,8 +17,8 @@ python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME -
 
 ## noun_adj
 
-MODEL_TYPE = "bert-bytoken"
-COMPARISON = "noun_adj"
+MODEL_TYPE="bert-bytoken"
+COMPARISON="noun_adj"
 
 
 #python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/bytoken_nounadj_lr5e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_epochs --save_model --dataset addone --learning_rate 5e-5 --comparison $COMPARISON
@@ -30,7 +30,7 @@ python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME -
 
 
 ## noun_noun
-COMPARISON = "noun_noun"
+COMPARISON="noun_noun"
 ':
 python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/bytoken_nounnoun_lr5e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_model --dataset addone --learning_rate 5e-5 --comparison $COMPARISON
 python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/bytoken_nounnoun_lr3e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_model --dataset addone --learning_rate 3e-5 --comparison $COMPARISON
@@ -42,7 +42,7 @@ python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME -
 
 ## noun-adjnoun
 
-COMPARISON = "noun_adjnoun"
+COMPARISON="noun_adjnoun"
 ':
 python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/bytoken_noun-adjnoun_lr5e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_model --dataset addone --learning_rate 5e-5 --comparison $COMPARISON
 python finetuning.py --model_type $MODEL_TYPE --model_name_or_path $MODEL_NAME --output_dir FTresults_Addone/bytoken_noun-adjnoun_lr3e-5/ --do_train --do_eval --evaluate_during_training --per_gpu_train_batch_size 8 --num_train_epochs 5 --save_preds --save_model --dataset addone --learning_rate 3e-5 --comparison $COMPARISON
